a528deddf9782b30b70a087f6ede399f71df9fbe
yelumala <104664965+yelumala@users.noreply.github.com>
2022-05-02 16:12:18 +0530

    Update ProjectManagementApp.cpp
    Adding more comments, using typedef for clock, Adding one more task status, adding methods for creating tasks, assigning user & resources to tasks, retrieving task from project

--- ProjectManagementApp.cpp
+++ ProjectManagementApp.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+typedef std::chrono::time_point<std::chrono::system_clock> Clock;
+
 // User details
 class User
 {
@@ -52,6 +54,7 @@ enum TaskStatus
     NEW = 0,
     TODO,
     INPROGRESS,
+    ONHOLD,
     DONE
 };
 
@@ -63,15 +66,39 @@ class Task
         Task(
             std::string pName,
             double pTimeToComplete,
-            TaskStatus pStatus,
-            Task *pLinkedTask,
-            User *pUser,
-            Resource &pResource);
-    
+            TaskStatus pStatus = TaskStatus::NEW,
+            Task *pLinkedTask = nullptr,
+            User *pUser = nullptr,
+            Resource *pResource = nullptr);
+            
+        // Setters for assigning start and end date
+        void setStartDate(Clock pStartDate);
+        void setEndDate(Clock pEndDate);
+        
+        // Getters for start and end date
+        Clock getStartDate();
+        Clock getEndDate();
+        
+        // Sets the status of a task
+        void setStatus(TaskStatus pStatus);
+        // returns status of a task
+        TaskStatus getStatus();
+        
+        // Links another task to the current Task
+        // The link can be a blocking link or relates to link
+        void linkTask(Task *pTask, LinkType pType);
+        
+        // Assigns an user to this task
+        void assignUser(User *pUser);
+        
+        // Assigns a Resource to this task
+        void assignResource(Resource *pResource);
+        
     private:
         std::string mName;
-        std::chrono::time_point<std::chrono::system_clock> mStartTime;
-        std::chrono::time_point<std::chrono::system_clock> mEndTime;
+        double mTimeToComple;
+        Clock mStartTime;
+        Clock mEndTime;
         TaskStatus mStatus;
         Task *mLinkedTask;
         User *mUser;
@@ -83,11 +110,32 @@ class Task
 class Project
 {
     public:
-        bool canCompleteBy(std::chrono::time_point<std::chrono::system_clock> pEndDate);
-    
+        Project(std::string pName, Clock pStartTime, Clock pEndTime);
+        
+        // Checks if the project can be completed by given pEndDate
+        // If all the tasks can be completed within the specified date adhering
+        // to their completion time, then project can be completed.
+        bool canCompleteBy(Clock pEndDate);
+        
+        // Setters for start and end date
+        void setStartDate(Clock pStartDate);
+        void setEndDate(Clock pEndDate);\
+        
+        // Getters for start and end date
+        Clock getStartDate();
+        Clock getEndDate();
+        
+        // Creates a task with specified name and time to complete the task
+        // The created task is added to list of tasks (mTasks)
+        void CreateTask(std::string pName, double pTimeToComplete);
+        
+        // Finds a task by its name
+        Task *getTask(std::string pName);
+
+        
     private:
         std::string mName;
+        Clock mStartTime;
+        Clock mEndTime;
         std::vector<Task> mTasks;
-        std::chrono::time_point<std::chrono::system_clock> mStartTime;
-        std::chrono::time_point<std::chrono::system_clock> mEndTime;
 };
